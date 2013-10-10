3af97fc083311aadebea8f3997c6b1702b53942e
Andy <Andy@Andy-Desktop>
2013-10-10 00:40:36 -0500

    added shit

--- src/dbaccess/MemberService.java
+++ src/dbaccess/MemberService.java
@@ -58,39 +58,4 @@ public class MemberService {
         memberDAO.deleteMember(member);
     }
     
-    
-//    /**
-//     *
-//     * @param args
-//     * @throws DataAccessException
-//     */
-//    public static void main(String[] args) throws DataAccessException {
-//        
-//        MemberService ms = new MemberService();
-//        Member member = new Member();
-//        
-//        member = ms.getMemberById("2");
-//        System.out.println(member.toString());
-//        System.out.println(ms.getAllMembers());
-//        
-//            member.setMemberId(12);
-//            member.setLastName("Goon");
-//            member.setFirstName("Bobby");
-//            member.setAddress("909 Main St.");
-//            member.setCity("Kenosha");
-//            member.setState("WI");
-//            member.setZip("53149");
-//            member.setPhone("262-719-3232");
-//            member.setEmail("goon@goon.com");
-//            member.setJoinDate(new Date());
-//            member.setMembershipType("1sin");
-//            member.setMembershipStatus("1act");
-//            member.setMembershipPayment("1mon");
-//            member.setFreeSession("1una");
-//            
-//            ms.saveMember(member);
-//            
-//            System.out.println(member.toString());
-//    }
-
 }
